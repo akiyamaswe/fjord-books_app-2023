a39bcf10fe0aa9c70b71165889d16686af2225f0
akiyama <ryo.akiyama112345@gmail.com>
2024-11-08 11:38:57 +0900

    テストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -6,4 +6,12 @@ class ReportTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'editable?' do
+    alice = users(:alice)
+    bob = users(:bob)
+    alice_report = reports(:alice_report)
+    assert alice_report.editable?(alice)
+    assert_not alice_report.editable?(bob)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should return email when name empty' do
+    user = User.new(email: 'akiyama@example.com', name: '')
+    assert_equal 'akiyama@example.com', user.name_or_email
+
+    user.name = 'akiyama'
+    assert_equal 'akiyama', user.name_or_email
+  end
 end
