3de4667417fbd8b3ac83bfbd8d9ef9210716356c
akiyama <ryo.akiyama112345@gmail.com>
2024-12-02 10:41:47 +0900

    JSONなどフォーマット指定での表示分けをしないのでrespond_toを削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -28,32 +28,26 @@ class ReportsController < ApplicationController
     @report = Report.new(report_params)
     @report.user = current_user
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /reports/1
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /reports/1
   def destroy
     @report.destroy!
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
-    end
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
